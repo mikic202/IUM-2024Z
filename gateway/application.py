962a6fceb092efceaffb01ca87aba8cf4f61980b
mikic202 <mikolaj.chomanski@gmail.com>
2024-12-14 13:40:40 +0100

    Add connection between containers

--- gateway/application.py
+++ gateway/application.py
@@ -3,17 +3,19 @@ from typing import List, Tuple
 import requests
 import json
 
+PREDICTION_TYPES = ["classification", "regression"]
+
 
 def create_application() -> Flask:
     app = Flask(__name__)
 
-    @app.route("/api/<resource_type>", methods=["POST"])
-    def post_resource(resource_type):
-        return resource_type
-
-    @app.route("/api", methods=["GET"])
+    @app.route("/api/status", methods=["GET"])
     def get_resources():
-        return "Basic API Gateway"
+        return requests.get("http://model_api:8080/ping").text
+
+    @app.route("/api/predict/<prediction_type>", methods=["POST"])
+    def predict(prediction_type):
+        return "!! NOT IMPLEMENTED !!"
 
     return app
 
