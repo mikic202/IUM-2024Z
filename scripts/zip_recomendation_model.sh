99b5af124b8d4ad5c31a543d7800fbe644dafa35
mikic202 <mikolaj.chomanski@gmail.com>
2024-12-21 21:15:30 +0100

    Add simple scripts for ziping models to .mar

---
+++ scripts/zip_recomendation_model.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p model_api/recomendation_models
+
+if [ "$2" = "force" ]; then
+    COUNT=$(ls model_api/recomendation_models | wc -l)
+    NUMBER_OF_VERSIONS=$(( COUNT - 1 > 0 ? COUNT - 1 : 0 ))
+else
+    NUMBER_OF_VERSIONS=$(ls model_api/recomendation_models | wc -l)
+fi
+
+MODEL_NAME="track_embeding_v$NUMBER_OF_VERSIONS"
+
+torch-model-archiver --model-name $MODEL_NAME \
+    --version $NUMBER_OF_VERSIONS \
+    --model-file model_builder/ModelHandler.py \
+    --serialized-file $1 \
+    --handler model_builder/ModelHandler.py \
+    --force \
+    --export-path model_api/recomendation_models
