a1718bcb20b60823254e1f29910bb94fccc92f4e
mikic202 <mikolaj.chomanski@gmail.com>
2024-12-23 22:43:43 +0100

    Fix trochserve comand to load latest model

--- model_api/model_start.sh
+++ model_api/model_start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-torchserve --start --model-store . --models test_model=test_model.mar --ts-config config.properties --disable-token-auth
+MODEL_DIR="track_embeding_models"
+
+HIGHEST_VERSION_FILE=$(ls track_embeding_models | sort -V | tail -1)
+
+echo "Starting model server with model $HIGHEST_VERSION_FILE"
+
+torchserve --start --model-store track_embeding_models --models embeding_model="$HIGHEST_VERSION_FILE" --ts-config config.properties --disable-token-auth
 
 # prevent docker exit
 tail -f /dev/null
