55b955ea45e9df1303075d543e7a4d367050a787
mikic202 <mikolaj.chomanski@gmail.com>
2024-12-23 18:27:46 +0100

    update script to use new handlers

--- scripts/zip_track_embedings.sh
+++ scripts/zip_track_embedings.sh
@@ -13,8 +13,8 @@ MODEL_NAME="track_embeding_v$NUMBER_OF_VERSIONS"
 
 torch-model-archiver --model-name $MODEL_NAME \
     --version $NUMBER_OF_VERSIONS \
-    --model-file model_builder/ModelHandler.py \
+    --model-file model_builder/TrackEmbedingsHandler.py \
     --serialized-file $1 \
-    --handler model_builder/ModelHandler.py \
+    --handler model_builder/TrackEmbedingsHandler.py \
     --force \
     --export-path model_api/track_embeding_models
