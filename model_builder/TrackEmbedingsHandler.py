e05cb785641dd5f2964b9eaa89d129faa0a06f4d
mikic202 <mikolaj.chomanski@gmail.com>
2024-12-23 18:21:48 +0100

    Add basic track embeding model handler

---
+++ model_builder/TrackEmbedingsHandler.py
@@ -0,0 +1,52 @@
+import torch
+import os
+import logging
+import pandas as pd
+
+
+class TrackEmbedingsHandler:
+    def __init__(self):
+        self._context = None
+        self.initialized = False
+        self.model = None
+        self.device = None
+
+    def initialize(self, context):
+        self.manifest = context.manifest
+
+        properties = context.system_properties
+        model_dir = properties.get("model_dir")
+        self.device = torch.device(
+            "cuda:" + str(properties.get("gpu_id"))
+            if torch.cuda.is_available()
+            else "cpu"
+        )
+        logging.info(f"TrackEmbedingsHandler initializing using: {str(self.device)}")
+
+        serialized_file = self.manifest["model"]["serializedFile"]
+        model_pt_path = os.path.join(model_dir, serialized_file)
+        if not os.path.isfile(model_pt_path):
+            raise RuntimeError("Missing the model.pt file")
+
+        self.model = torch.jit.load(model_pt_path)
+
+        self.initialized = True
+        logging.info(f"TrackEmbedingsHandler initialized")
+
+    def preprocess(self, data):
+        input_data = pd.Series(data[0].get("body"))
+
+        unpacked_data = []
+        for data in input_data.drop("id_track").values:
+            if type(data) != list:
+                unpacked_data.append(data)
+            else:
+                unpacked_data += data
+
+        tensor_data = torch.tensor(unpacked_data)
+        return tensor_data
+
+    def handle(self, data, _):
+        data = self.preprocess(data)
+        pred_out = self.model.forward(data)
+        return [pred_out.tolist()]
