ac461bfa24962fcf79885bc2143dc4123fd0d1b1
mikic202 <mikolaj.chomanski@gmail.com>
2024-12-25 20:58:23 +0100

    Add second model to torchserve

--- model_api/model_start.sh
+++ model_api/model_start.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+MAIN_MODEL_STORE=main_models
+
 MODEL_DIR="track_embeding_models"
+RECOMANDATIONS_MODEL_DIR="recomendation_models"
 
 HIGHEST_VERSION_FILE=$(ls track_embeding_models | sort -V | tail -1)
+HIGHEST_RECOMENDATION_VERSION_FILE=$(ls recomendation_models | sort -V | tail -1)
 
 echo "Starting model server with model $HIGHEST_VERSION_FILE"
+echo "Starting recomandation server with model $HIGHEST_RECOMENDATION_VERSION_FILE"
 
-torchserve --start --model-store track_embeding_models --models embeding_model="$HIGHEST_VERSION_FILE" --ts-config config.properties --disable-token-auth
+mkdir -p $MAIN_MODEL_STORE
+cp -r $MODEL_DIR/$HIGHEST_VERSION_FILE $MAIN_MODEL_STORE/
+cp -r $RECOMANDATIONS_MODEL_DIR/$HIGHEST_RECOMENDATION_VERSION_FILE $MAIN_MODEL_STORE/
+
+torchserve --start --model-store $MAIN_MODEL_STORE --models embeding_model="$HIGHEST_VERSION_FILE" recomendations_model="$HIGHEST_RECOMENDATION_VERSION_FILE" --ts-config config.properties --disable-token-auth --enable-model-api
 
 # prevent docker exit
 tail -f /dev/null
